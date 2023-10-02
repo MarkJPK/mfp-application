59347ea66c8897ea8871189f0ac41b4d011876b3
Mark Keely <markkeely@btinternet.com>
2023-10-02 21:49:06 +0100

    Make change.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Marketing App</h1>
+      <h1>Marketing Application</h1>
       <hr />
       <MarketingApp />
     </div>
