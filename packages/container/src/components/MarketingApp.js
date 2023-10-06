f91936017584c99edf1c7c6005627f933b260599
Mark Keely <markkeely@btinternet.com>
2023-10-06 16:52:10 +0100

    Add routing (broken)

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,34 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   //  Run the following function once when the component mounts
   useEffect(() => {
-    mount(ref.current);
-  });
+    //  Mount the application
+    const { onParentNavigate } = mount(ref.current, {
+      //  Provide a callback that will be invoked whenever the browser history changes
+      //  with the child application
+      onNavigate: ({ pathname: nextPathname }) => {
+        //  Get the current pathname
+        const { currentPathname } = history.location;
+
+        //  Make sure the current and next pathnames are different
+        //  to prevent an infinite loop
+        if (nextPathname !== currentPathname) {
+          //  Navigate to the next pathname
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    //  Provide the onParentNavigate function to the history so that it
+    //  is invoked whenever the history is updated
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 //  Render the application into the given element
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  //  Create a memory history
+  const history = defaultHistory || createMemoryHistory();
+
+  //  Provide the onNavigate function to the history so that it
+  //  is invoked whenever the history is updated
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  //  Render the application and pass it the memory history
+  ReactDOM.render(<App history={history} />, el);
+
+  //  Return callbacks to the parent application
+  return {
+    //  Return a callback to be invoked whenever the parent application
+    //  navigates to a URL
+    onParentNavigate({ pathname: nextPathname }) {
+      //  Get the current pathname
+      const { currentPathname } = history.location;
+
+      //  Make sure the current and next pathnames are different
+      //  to prevent an infinite loop
+      if (nextPathname !== currentPathname) {
+        //  Navigate to the next pathname
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 //  Context / Situation #1
@@ -21,7 +49,9 @@ if (process.env.NODE_ENV === 'development') {
   if (devRoot) {
     //  The element was found so it is assumed that the application is running in isolation
     //  Mount the application into the element
-    mount(devRoot);
+    mount(devRoot, {
+      defaultHistory: createBrowserHistory(),
+    });
   }
 }
 
